c556dd8dd1cfe65d799aa3245a189039ded701f9
fpezcara <fpezcara@gmail.com>
2019-11-13 22:43:24 +0000

    primer commit

---
+++ piedra-papel-tijera.js
@@ -0,0 +1,77 @@
+// // If else
+// Hacer un programa que le permita a dos usuarios jugar al piedra, papel o tijera
+// El primer usuario tiene que ingresar su eleccion
+// Si no ingresa piedra, papel o tijera le decimos que no puede jugar
+// Despues el segundo usuario
+// Les respondemos quien gano .
+// consejo de Male: Empecemos por un caso. Por ej si uno escribe piedra y el otro tijera
+
+let usuario1;
+let usuario2;
+let papel = "papel";
+let tijera = "tijera";
+let piedra = "piedra";
+let puntajeJugador1 = 2;
+let puntajeJugador2 = 2;
+
+
+alert("¡Bienvenidx a PIEDRA, PAPEL o TIJERA!")
+
+usuario1 = prompt("USUARIO 1: Por favor ingrese una opción: piedra, papel o tijera");
+usuario2 = prompt("USUARIO 2: Por favor ingrese una opción: piedra, papel o tijera");
+
+if (usuario1 === tijera && usuario2 === papel) {
+    alert(`¡Usuario 1 ganaste este turno!`)
+    puntajeJugador2--
+}
+
+else if (usuario1 === tijera && usuario2 === piedra) {
+    alert(`¡Usuario 2 ganaste este turno!`)
+    puntajeJugador1--
+}
+
+else if (usuario1 === tijera && usuario2 === tijera) {
+    alert(`¡Esto es un EMPATE!`)
+}
+
+else if (usuario1 === piedra && usuario2 === papel) {
+    alert(`¡Usuario 2 ganaste este turno!`)
+    puntajeJugador1--
+}
+
+else if (usuario1 === piedra && usuario2 === piedra) {
+    alert(`¡Esto es un EMPATE!`)
+}
+
+else if (usuario1 === piedra && usuario2 === tijera) {
+    alert(`Usuario 1 ganaste este turno!`)
+    puntajeJugador2--
+}
+
+else if (usuario1 === papel && usuario2 === papel) {
+    alert(`¡Esto es un EMPATE!`)
+}
+
+else if (usuario1 === papel && usuario2 === piedra) {
+    alert(`¡Usuario 1 ganaste este turno!`)
+    puntajeJugador2--
+}
+
+else if (usuario1 === papel && usuario2 === tijera) {
+    alert(`Usuario 2 ganaste este turno!`)
+    puntajeJugador1--
+}
+
+else {
+    alert(`¡Lo siento! No ingresaste nada`)
+}
+// puntajeJugador1 && puntajeJugador2 === i
+
+let i = 0;
+
+total = (puntajeJugador1 + puntajeJugador2);
+
+for (i = total; i <= 2; i--) {
+    console.log(i)
+}
+    
